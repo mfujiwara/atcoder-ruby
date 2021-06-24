12d5a6546c35dff7c0424bc308a966d2a95055b5
Mikihiro Fujiwara <lightpurplewisteria@gmail.com>
2021-06-25 00:05:05 +0900

    AC

---
+++ abc006/4.py
@@ -0,0 +1,44 @@
+class SegmentTree:
+    def __init__(self, init_val, seg_func, ide_ele):
+        size = len(init_val)
+        self.seg_func = seg_func
+        self.ide_ele = ide_ele
+        self.n = 1 << (size - 1).bit_length()
+        self.node = [ide_ele] * 2 * self.n
+        for i in range(size):
+            self.node[self.n + i - 1] = init_val[i]
+        for i in range(self.n - 2, -1, -1):
+            self.node[i] = self.seg_func(self.node[2 * i + 1], self.node[2 * i + 2])
+
+    def update(self, index, val):
+        i = index + self.n - 1
+        self.node[i] = val
+        while i > 0:
+            i = int((i - 1)//2)
+            l = self.node[2*i+1]
+            r = self.node[2*i+2]
+            self.node[i] =  self.seg_func(l, r)
+  
+    def query(self, left, right):
+        ret=self.ide_ele
+        left+=self.n-1
+        right+=self.n-1
+        while left<right:
+            if left%2==0:
+                ret = self.seg_func(ret, self.node[left])
+                left+=1
+            if right%2==0:
+                right-=1
+                ret = self.seg_func(ret, self.node[right])
+            left=int(left//2)
+            right=int(right//2)
+        return ret
+def seg_func(a,b):
+    return max(a,b)
+N=int(input())
+tree=SegmentTree([0]*(N+1),seg_func,0)
+for _ in range(N):
+    c=int(input())
+    d=tree.query(0,c)
+    tree.update(c,d+1)
+print(N-tree.query(0,N+2))
