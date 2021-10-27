68caea56d549b53d48cfd6215a2973ef9fede424
Mikihiro Fujiwara <lightpurplewisteria@gmail.com>
2021-10-27 14:33:39 +0900

    AC

--- arc128/c.py
+++ arc128/c.py
@@ -1,17 +1,15 @@
 import itertools
 N,M,S=map(int, input().split())
 array=list(map(int, input().split()))
+sums=[0]+list(itertools.accumulate(array))
 ret=0
-while S>0:
-    sums=list(itertools.accumulate(array[::-1]))[::-1]
-    maxi=(array[-1],len(array)-1)
-    for i in range(2,len(array)+1):
-        tmp=sums[-i]/i
-        if tmp>maxi[0]:
-            maxi=(tmp,len(array)-i)
-    v,index=maxi
-    heights=min(M*(len(array)-index),S)
-    ret+=v*heights
-    S-=heights
-    array=array[:index]
+for i in range(N):
+    for j in range(i,N+1):
+        x2=M
+        x1=(S-x2*(N-j))
+        if i<j:
+            x1/=(j-i)
+        if x1<0 or x1>=M: continue
+        r=x2*(sums[N]-sums[j])+x1*(sums[j]-sums[i])
+        ret=max(ret,r)
 print(ret)
