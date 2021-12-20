11ac4dce018420a4a530cdbf6267eab1e2fa5926
Mikihiro Fujiwara <lightpurplewisteria@gmail.com>
2021-12-21 00:15:03 +0900

    AC

---
+++ arc029/3.py
@@ -0,0 +1,52 @@
+class UnionFind:
+    def __init__(self, size):
+        self.rank = [0]*size
+        self.parent = [i for i in range(size)]
+
+    def unite(self, id_x, id_y):
+        x_parent = self.get_parent(id_x)
+        y_parent = self.get_parent(id_y)
+        if x_parent == y_parent:
+            return 
+        if self.rank[x_parent] > self.rank[y_parent]:
+            self.parent[y_parent] = x_parent
+        else:
+            self.parent[x_parent] = y_parent
+            if self.rank[x_parent] == self.rank[y_parent]:
+                self.rank[y_parent] += 1 
+
+    def get_parent(self, id_x):
+        if self.parent[id_x] == id_x:
+            return id_x
+        else:
+            self.parent[id_x] = self.get_parent(self.parent[id_x])
+            return self.parent[id_x]
+
+    def is_same_parent(self, id_x, id_y):
+        return self.get_parent(id_x) == self.get_parent(id_y)
+
+    def size(self):
+        s=set()
+        for x in self.parent:
+            s.add(self.get_parent(x))
+        return len(s)
+N,M=map(int, input().split())
+costs=[]
+for i in range(N):
+    c=int(input())
+    costs.append((c,i+1,0))
+for _ in range(M):
+    a,b,r=map(int, input().split())
+    costs.append((r,a,b))
+costs.sort()
+union=UnionFind(N+1)
+count=0
+ret=0
+for c,a,b in costs:
+    if union.is_same_parent(a,b): continue
+    union.unite(a,b)
+    count+=1
+    ret+=c
+    if count==N:
+        break
+print(ret)
