7221e96f26dc01f7f9fadbd268c8a7fc62df612b
Mikihiro Fujiwara <lightpurplewisteria@gmail.com>
2022-03-19 03:02:09 +0900

    fix

--- arc016/3.py
+++ arc016/3.py
@@ -11,18 +11,17 @@ for _ in range(M):
         dd[idol-1]=p
     idols.append(dd)
 INF=pow(10,10)
-dp=[INF]*pow(2,N)
-dp[0]=0
-for bit in range(pow(2,N)):
+dp=[INF]*pow(2,N) # その状態から全部揃えるのにかかる金額の期待値
+dp[-1]=0
+for bit in range(pow(2,N)-2,-1,-1):
     for i in range(M):
-        y=100
-        x=0
+        x=0 # 遷移後の期待値の合計
+        y=0 # 他の状態に遷移する確率
         for k,p in idols[i].items():
-            if (bit>>k)&1==0:
-                y -= p
-            else:
-                ns = bit^(1<<k)
-                x+=p*dp[ns]
-        if y > 0:
-            dp[bit]=min(dp[bit], x/y+costs[i]*100/y)
-print(dp[-1])
+            if bit&(1<<k)==0:
+                x+=p*dp[bit|(1<<k)]
+                y+=p
+        if y>0:
+            # 遷移後のコストの期待値 + 他の状態に遷移するのにかかるコストの期待値
+            dp[bit]=min(dp[bit],x/y+costs[i]*100/y)
+print(dp[0])
