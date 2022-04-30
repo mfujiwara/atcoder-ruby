e6535a0475469497eb9fa8ac4f81386b75ab63dc
Mikihiro Fujiwara <lightpurplewisteria@gmail.com>
2022-04-30 16:20:34 +0900

    AC

---
+++ abc039/c.py
@@ -0,0 +1,15 @@
+S=input()[:12]
+if S=="WBWBWWBWBWBW":
+    print("Do")
+elif S=="WBWWBWBWBWWB":
+    print("Re")
+elif S=="WWBWBWBWWBWB":
+    print("Mi")
+elif S=="WBWBWBWWBWBW":
+    print("Fa")
+elif S=="WBWBWWBWBWWB":
+    print("So")
+elif S=="WBWWBWBWWBWB":
+    print("La")
+elif S=="WWBWBWWBWBWB":
+    print("Si")
