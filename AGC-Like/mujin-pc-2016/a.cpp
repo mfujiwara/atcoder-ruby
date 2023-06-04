5899b269deafc5e438fdc697b0ec73d8e1451b80
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-06-05 00:35:36 +0900

    AC

---
+++ AGC-Like/mujin-pc-2016/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string c;
+    cin >> c;
+    if (c=="O" || c=="P" || c=="K" || c=="L") {
+        cout << "Right" << endl;
+    } else {
+        cout << "Left" << endl;
+    }
+}
