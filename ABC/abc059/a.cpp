2017f33dc6d7beb5a19ab8e03edd0155b421e9f8
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-06-26 02:56:40 +0900

    AC

---
+++ ABC/abc059/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1,s2,s3;
+    cin >> s1 >> s2 >> s3;
+    char c1 = s1[0] + 'A' - 'a';
+    char c2 = s2[0] + 'A' - 'a';
+    char c3 = s3[0] + 'A' - 'a';
+    cout << c1 << c2 << c3 << endl;
+}
