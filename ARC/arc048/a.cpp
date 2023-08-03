ecc10006e75ca434596a879808c5db882a5f6cfe
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-08-04 01:54:20 +0900

    AC

---
+++ ARC/arc048/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long A,B;
+    cin >> A >> B;
+    if (A*B>0) {
+        cout << B-A << endl;
+    } else {
+        cout << B-A-1 << endl;
+    }
+}
