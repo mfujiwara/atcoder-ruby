542d558bebca71d7b0cca1599737da2b0dd2a63b
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-10-17 04:39:09 +0900

    AC

---
+++ ABC/abc065/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long X,A,B;
+    cin >> X >> A >> B;
+    if (B<=A) {
+        cout << "delicious" << endl;
+    } else if (B<=A+X) {
+        cout << "safe" << endl;
+    } else {
+        cout << "dangerous" << endl;
+    }
+}
