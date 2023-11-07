dd107706799b34773126d66347691b87b5ce2d74
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-11-08 00:43:11 +0900

    AC

---
+++ ARC/arc016/1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N,M;
+    cin >> N >> M;
+    if (M==1) {
+        cout << 2 << endl;
+    } else {
+        cout << 1 << endl;
+    }
+}
