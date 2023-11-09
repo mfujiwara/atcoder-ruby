01eb43f70616adc5f3747c65f9a933b105126244
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-11-10 08:08:05 +0900

    AC

---
+++ ARC/arc050/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char C,c;
+    cin >> C >> c;
+    if (C-'A'==c-'a') {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
