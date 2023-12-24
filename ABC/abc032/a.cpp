a4c1f5e827f1200e9496252d75e91360657da8b3
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2023-12-25 08:24:58 +0900

    AC

---
+++ ABC/abc032/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,n;
+    cin >> a >> b >> n;
+    while (n%a!=0 || n%b!=0) {
+        n++;
+    }
+    cout << n << endl;
+}
