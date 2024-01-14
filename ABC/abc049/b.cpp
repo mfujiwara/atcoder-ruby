f41410dc5bb98c4eeecbbb42cddc99a1f3ee9814
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-01-15 01:07:32 +0900

    AC

---
+++ ABC/abc049/b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int H,W;
+    cin >> H >> W;
+    string S;
+    for (int i=0;i<H;i++) {
+        cin >> S;
+        cout << S << endl << S << endl;
+    }
+}
