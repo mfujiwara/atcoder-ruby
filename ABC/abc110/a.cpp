28191b7c7be740f67d9abbf6b3465fe1ef64244c
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-03-29 02:19:47 +0900

    AC

---
+++ ABC/abc110/a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> abc(3);
+    cin >> abc[0] >> abc[1] >> abc[2];
+    sort(abc.begin(), abc.end());
+    cout << abc[0]+abc[1]+abc[2]*10 << endl;
+}
