c1627cc914bef7fbfae38c842c266de14fd07924
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-03-30 15:03:50 +0900

    AC

---
+++ ABC/abc126/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N,K;
+    string S;
+    cin >> N >> K >> S;
+    for (int i=0;i<N;i++) {
+        if (i == K-1) {
+            cout << (char)(S[i]+32);
+        } else {
+            cout << S[i];
+        }
+    }
+    cout << endl;
+}
