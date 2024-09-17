1811334b6edb48fd7eed20fa137fdd73e07dc842
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-09-17 20:05:23 +0900

    AC

---
+++ PAST/past202212-open/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_pbds;
+
+int main() {
+    long long N,X,Y;
+    cin >> N >> X >> Y;
+    cout << N/X*Y << endl;
+}
