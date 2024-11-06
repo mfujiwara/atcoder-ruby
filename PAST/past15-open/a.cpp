cf826e1ee6353f24f22f53968126035948472063
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-11-06 14:39:14 +0900

    AC

---
+++ PAST/past15-open/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_pbds;
+
+int main() {
+    int A,T;
+    cin >> A >> T;
+    cout << A*5+T << endl;
+}
