de1c8e11fb443a500eee8d32c566c59144a8f0ca
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-11-07 02:16:50 +0900

    AC

---
+++ PAST/past15-open/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_pbds;
+
+int main() {
+    int C,H;
+    cin >> C >> H;
+    if (H>=2800) {
+        cout << "o" << endl;
+    } else {
+        cout << "x" << endl;
+    }
+}
