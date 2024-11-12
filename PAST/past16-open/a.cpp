f0078a7897c50120690204fc7d31839bf0653cce
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-11-12 22:54:57 +0900

    AC

---
+++ PAST/past16-open/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_pbds;
+
+int main() {
+    int M;
+    cin >> M;
+    if (4<=M && M<=9) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
