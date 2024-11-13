1a4eddcc966177229f1ab8034c11375a83ece16d
Mikihiro Fujiwara <mikihiro-fujiwara@m3.com>
2024-11-14 03:28:15 +0900

    AC

---
+++ PAST/past16-open/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace __gnu_pbds;
+
+int main() {
+    int A,B,C;
+    cin >> A >> B >> C;
+    if (A==B && B==C) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
